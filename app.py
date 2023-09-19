b57aa3c201452218a771ecb4e35c59141b36205e
Lee00382 <119830988+Lee00382@users.noreply.github.com>
2023-09-18 23:51:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,79 @@
 import streamlit as st
-import pandas as pd
 import openai
-import os
+import pandas as pd
+import io
+import xlsxwriter
 
-openai.api_key = "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"
+api_key = "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"
 
-st.title("SAT Question Answering App")
+openai.api_key = api_key
 
-uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
+st.title("SAT Question Explanation Generator")
+st.write("Upload an Excel file with SAT questions, and this tool will generate explanations for each question.")
 
-if uploaded_file:
+def generate_explanation(question, answer_a, answer_b, answer_c, answer_d, correct_answer_choice):
+    user_prompt = f"Explain the following SAT question: {question}\n"
+    user_prompt += f"a) {answer_a}\n"
+    user_prompt += f"b) {answer_b}\n"
+    user_prompt += f"c) {answer_c}\n"
+    user_prompt += f"d) {answer_d}\n"
+    user_prompt += f"The correct answer is choice {correct_answer_choice}."
+
+    system_prompt = "You are a helpful SAT assistant for making clear and succinct explanations given a question and correct answer choice. You should give why the correct answer is correct and why other choices are incorrect. If the question is short answer question, just give how you got to the correct answer. Please be concise in your response."
+    conversation = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=conversation,
+    )
+
+    explanation = response['choices'][0]['message']['content']
+    return explanation
+
+def process_excel_file(file):
     try:
-        df = pd.read_excel(uploaded_file, engine="openpyxl")
-
-        st.write("Data from Excel file:")
-        st.write(df)
-
-        st.header("Ask a Question")
-
-        user_question = st.text_input("Enter your question:")
-
-        if user_question:
-            response = openai.Completion.create(
-                engine="text-davinci-002",  
-                prompt=f"Data: {df.to_string()}\nQuestion: {user_question}\nAnswer:",
-                max_tokens=1000
+        df = pd.read_excel(file)
+        explanations = []
+        for _, row in df.iterrows():
+            explanation_text = generate_explanation(
+                row['Question'],
+                row['Answer_A'],
+                row['Answer_B'],
+                row['Answer_C'],
+                row['Answer_D'],
+                row['Correct_Answer_Choice']
             )
-            
-            chatbot_answer = response.choices[0].text.strip()
-            st.write(f"Answer: {chatbot_answer}")
-            
-            if st.button("Export to Excel"):
-                df = pd.DataFrame({"Question": [user_question], "Answer": [chatbot_answer]})
+            explanations.append(explanation_text)
+        df['Explanation'] = explanations
+        return df
+    except Exception as e:
+        return str(e)
 
-            excel_file_path = "chatbot_answer.xlsx"
-            df.to_excel(excel_file_path, index=False, engine="openpyxl")    
+uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
 
-            st.success("Answers exported to Excel")
+if uploaded_file is not None:
+    try:
+        st.write("Generating explanations...")
+        result_df = process_excel_file(uploaded_file)
+        st.subheader("Explanations for SAT Questions:")
+        st.dataframe(result_df)
+        
+        st.write("Live Editing:")
 
-
+        edited_response = st.data_editor(result_df)
+        
+        output = io.BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        edited_df.to_excel(writer, sheet_name='Explanations', index=False)
+        writer.save()
+        output.seek(0)
+        st.download_button(
+            label="Download Explanations and Questions (Excel)",
+            data=output,
+            file_name="sat_explanations_and_questions.xlsx",
+        )
     except Exception as e:
         st.error(f"An error occurred: {e}")
